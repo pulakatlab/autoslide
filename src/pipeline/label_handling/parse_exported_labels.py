bf0c03d2a4d8a52896e328a428b4650666dc3a98
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-03-10 18:06:40 -0400

    feat(label-handling): add functionality to overlay and mask images
    - Added support for loading image file paths and plotting polygon overlays on images.
    - Introduced plotting functionality to visualize overlays on original images.
    - Implemented mask creation from polygon data using PIL and numpy.
    - Enhanced the script with additional imports for handling image paths and plotting.

--- src/pipeline/label_handling/parse_exported_labels.py
+++ src/pipeline/label_handling/parse_exported_labels.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from tqdm import tqdm, trange
+import os
+from glob import glob
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import numpy as np
 
 export_json_path = '/home/abuzarmahmood/projects/pulakat_lab/auto_slide/data/labelled_images/Export_catalog_query_3_10_2025.ndjson'
 # export_json_path = '/home/abuzarmahmood/projects/pulakat_lab/auto_slide/data/labelled_images/Export_catalog_query_3_10_2025_1_img.ndjson'
@@ -28,3 +33,47 @@ for row_ind in trange(len(export_df)):
                 polygon_list.append(polygon)
 
 polygon_df = pd.DataFrame({'filename':filename_list, 'polygon':polygon_list})
+
+data_dir = '/home/abuzarmahmood/projects/pulakat_lab/auto_slide/data'
+
+# Find filepaths and plot overlays
+filenames = polygon_df['filename'].unique()
+path_list = []
+for this_name in filenames:
+    # Search for this_name in data_dir 
+    glob_str = os.path.join(data_dir, '**', this_name)
+    filepaths = glob(glob_str, recursive=True)[0]
+    path_list.append(filepaths)
+
+path_map = dict(zip(filenames, path_list))
+polygon_df['filepath'] = polygon_df['filename'].map(path_map)
+
+# Plot overlay
+ind = 0
+this_row = polygon_df.iloc[ind]
+this_filepath = this_row['filepath']
+this_polygon = this_row['polygon']
+this_img = plt.imread(this_filepath)
+poly_x = [x['x'] for x in this_polygon]
+poly_y = [x['y'] for x in this_polygon]
+plt.imshow(this_img)
+plt.plot(poly_x, poly_y, '-o', c='y')
+plt.show()
+
+# Create mask
+
+# polygon = [(x1,y1),(x2,y2),...] or [x1,y1,x2,y2,...]
+# width = ?
+# height = ?
+
+width = this_img.shape[1]
+height = this_img.shape[0]
+polygon = [(x,y) for x,y in zip(poly_x, poly_y)]
+img = Image.new('L', (width, height), 0)
+ImageDraw.Draw(img).polygon(polygon, outline=1, fill=1)
+mask = np.array(img)
+
+fig, ax = plt.subplots(1,2)
+ax[0].imshow(this_img)
+ax[1].imshow(mask)
+plt.show()
