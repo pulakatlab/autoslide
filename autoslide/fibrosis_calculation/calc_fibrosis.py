dea8ddf5cc7ded218275ccecb3ebc63f6790ed91
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-07-04 12:05:11 -0400

    feat(calc_fibrosis): integrate section visualization from SVS
    - Removed trailing whitespace from numpy import.
    - Added functionality to load and process tracking data.
    - Integrated section visualization using Matplotlib.
    - Implemented a test case using a specific hash to visualize a section from an SVS file.
    - Utilized utility functions to fetch sections based on hash and visualize them.

--- autoslide/fibrosis_calculation/calc_fibrosis.py
+++ autoslide/fibrosis_calculation/calc_fibrosis.py
@@ -6,7 +6,7 @@ import os
 import sys
 import slideio
 from matplotlib import pyplot as plt
-import numpy as np 
+import numpy as np
 import pandas as pd
 from glob import glob
 import json
@@ -14,4 +14,39 @@ from tqdm import tqdm
 from ast import literal_eval
 from autoslide import config
 from autoslide.pipeline import utils
+from autoslide.utils.get_section_from_hash import *
 
+
+##############################
+data_dir = config['data_dir']
+tracking_dir = os.path.join(data_dir, 'tracking')
+
+# Load tracking data
+suggested_regions_paths, basename_list, data_path_list = load_tracking_data(
+    tracking_dir)
+
+# Load suggested regions
+final_df = load_suggested_regions(
+    suggested_regions_paths, basename_list, data_path_list)
+
+# Test with a specific hash
+test_hash = '0cb8cf88e2d3c22d'
+section = get_section_from_hash(test_hash, final_df)
+
+section_bounds = literal_eval(section['section_bounds'])
+slide = slideio.open_slide(section['data_path'], 'SVS')
+scene = slide.get_scene(0)
+
+section = utils.get_section(scene, section_bounds, 1)
+
+if section is not None:
+    # Visualize the section
+    # fig, ax = visualize_section(section, utils)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    ax.imshow(section)
+    fig.suptitle(f"Section from SVS\nSection: {section['basename']}, Hash: {section['section_hash']}" +
+                 f"\nData Path: {section['data_path']},\nSection Bounds: {section['section_bounds']}")
+    plt.tight_layout()
+    plt.show()
+else:
+    print(f"No section found with hash {test_hash}")
